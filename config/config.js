9df8921ed7ec9a057a0e24bb3f385d8ae246fb0a
mdzubaidul <freelancerjami@gmail.co>
2022-06-09 14:45:44 +0600

    update

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const connectDb = async () => {
-  const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1wbmx.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+  // const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1wbmx.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 
-  // const URI = `mongodb://localhost:27017/${process.env.DB_DATABASE}`;
+  const URI = `mongodb://localhost:27017/${process.env.DB_DATABASE}`;
   try {
     const conn = await mongoose.connect(URI, {
       useUnifiedTopology: true,
